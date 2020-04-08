c439d3327487c181c21aab0fa9da745c7b73dccc
PP <hircus@luukku.com>
2020-04-08 08:13:46 +0300

    parserissa nyt kaikki operaatiot
    ei hyvä mutta välttää

--- aleator/AleatorParser.java
+++ aleator/AleatorParser.java
@@ -1,22 +1,69 @@
 package aleator;
 
+import java.util.Random;
+
 public class AleatorParser {
+    protected Random r;
 
     AleatorParser() {
-
+        r = new Random();
     }
     public float parse(String s) {
+        return parseForSum(s);
+//        return parseForSum(s);
+//        return parseForDie(s);
+    }
+    public float parseForSum(String s) {
         String[] parts = s.split("\\+");
-        float result;
-        if (parts.length > 1) {
-            result = 0;
-            for(String p : parts) {
-                result += this.parse(p);
-            }
-        } else {
-            result = Float.parseFloat(s);
+        float result = 0;
+        for(String p : parts) {
+            result += this.parseForDiff(p);
         }
         return result;
     }
-
+    public float parseForDiff(String s) {
+        String[] parts;
+        parts = s.split("\\-");
+        float result = this.parseForProduct(parts[0]);
+        for(int i=1; i<parts.length; i++) {
+            result -= this.parseForProduct(parts[i]);
+        }
+        return result;
+    }
+    public float parseForProduct(String s) {
+        String[] parts;
+        parts = s.split("\\*");
+        float result = this.parseForQuot(parts[0]);
+        for(int i=1; i<parts.length; i++) {
+            result *= this.parseForQuot(parts[i]);
+        }
+        return result;
+    }
+    public float parseForQuot(String s) {
+        String[] parts;
+        parts = s.split("\\÷");
+        float result = this.parseForDie(parts[0]);
+        for(int i=1; i<parts.length; i++) {
+            result /= this.parseForDie(parts[i]);
+        }
+        return result;
+    }
+    public float parseForDie(String s) {
+        String[] parts;
+        float result = 0;
+        parts = s.split("d");
+        if (parts.length == 2) {
+            int x = (int)Float.parseFloat(parts[0]);
+            int y = (int)Float.parseFloat(parts[1]);
+            for(int i=0; i<x; i++) {
+                result += rollDie(y);
+            }
+        } else {
+            result = Float.parseFloat(parts[0]);
+        }
+        return result;
+    }
+    public int rollDie(int sides) {
+        return this.r.nextInt(sides)+1;
+    }
 }
